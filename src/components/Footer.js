232379de75d88b6b9b5067ffb6755c2a810e21f0
Andressa Da Costa <andressadacostan@gmail.com>
2022-06-30 12:03:57 -0300

    footer and favicon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,6 +13,7 @@ import { FiAlertTriangle } from 'react-icons/fi'
 import '../styles/Footer.css'
 
 function Footer() {
+  let iconReactStyles = { color: 'aqua', fontSize: '1.5em' }
   return (
     <footer className="footer-container">
       <div className="links-footer">
@@ -34,11 +35,11 @@ function Footer() {
         >
           <BsXCircle className="icon-footer two" />
         </a>
-        {/* <p id="zero">0</p> */}
+        <p id="zero">|</p>
 
         <FiAlertTriangle className="icon-footer two" />
         <p className="text-footer">
-          Developed with ❤ and Code by Andressa Costa
+          Developed with ❤ and Code by Andressa Da Costa
         </p>
 
         <p id="info">| Ln25,Col58 Spaces:4 UTF-8</p>
@@ -47,7 +48,7 @@ function Footer() {
         <p id="prettier">Prettier</p>
         <BsBraces className="icon-footer two" />
         <p id="js">Javascript</p>
-        <DiReact className="icon-footer two" />
+        <DiReact style={iconReactStyles} className="icon-footer two" />
         <p id="react">React </p>
         <BsBell className="icon-footer two" />
       </div>
